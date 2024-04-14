3b262f9b8a09d0e6eb92f898d82a1ef00626cfe2
Tamicktom <rickpokemon38@gmail.com>
2024-04-14 00:18:54 -0300

    ok

--- curso-laravel/routes/web.php
+++ curso-laravel/routes/web.php
@@ -1,56 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProdutoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::any("/any", function () {
-    return "Any";
-});
-
-Route::match(["get", "post"], "/match", function () {
-    return "Match";
-});
-
-Route::get("/produto/{id}", function (Request $request, $id) {
-    return "Produto: {$id}";
-});
-
-Route::redirect("/sobre", "/empresa");
-
-
-// Route::get("/empresa", function () {
-//     return view("empresa");
-// });
-
-Route::view("/empresa", "empresa");
-
-
-Route::get("/news", function () {
-    return view("news");
-})->name("Noticias");
-
-Route::get("/novidades", function () {
-    return redirect()->route("Noticias");
-});
-
-
-Route::group([
-    "prefix" => "admin",
-    "as" => "admin."
-], function () {
-    Route::get("/dashboard", function () {
-        return "Dashboard";
-    })->name("dashboard");
-
-    Route::get("/financeiro", function () {
-        return "Financeiro";
-    })->name("financeiro");
-
-    Route::get("/produtos", function () {
-        return "Produtos";
-    })->name("produtos");
-});
+Route::get('/', [ProdutoController::class, "index"]);
