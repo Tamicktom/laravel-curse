4aa548a19d2359939534e252160c4dbc3e1427d8
Tamicktom <rickpokemon38@gmail.com>
2024-04-13 23:02:41 -0300

    salvando

--- curso-laravel/routes/web.php
+++ curso-laravel/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/empresa", function () {
+    return view("empresa");
+});
+
+Route::any("/any", function () {
+    return "Any";
+});
+
+Route::match(["get", "post"], "/match", function () {
+    return "Match";
+});
